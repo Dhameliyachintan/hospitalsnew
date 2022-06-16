f4dca2043044fc89db00554370c7f681a28bdfd5
Dhameliyachintan <chintandhameliya07@gmail.com>
2022-06-16 23:28:21 +0530

    hospitals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './container/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Switch } from 'react-router-dom';
+import About from './container/About';
+import Contact from './container/Contact';
+import Departments from './container/Departments';
+import Doctors from './container/Doctors';
+import Appointment from './container/Appointment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Switch>
+        <Route exact path={"/"} component={Home} />
+        <Route exact path={"/about"} component={About}/>
+        <Route exact path={"/contact"} component={Contact}/>
+        <Route exact path={"/departments"} component={Departments}/>
+        <Route exact path={"/doctors"} component={Doctors}/>
+        <Route exact path={"/Appointment"} component={Appointment}/>
+      </Switch>
+      <Home/>
+      <Footer/>
+    </>
   );
 }
 
