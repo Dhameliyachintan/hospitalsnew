388c2091cdecd3a811b27f91605ef75446708ab0
Dhameliyachintan <chintandhameliya07@gmail.com>
2022-08-01 11:06:30 +0530

    01-08

--- src/common/api/Auth.api.js
+++ src/common/api/Auth.api.js
@@ -1,23 +1,36 @@
-// import { createUserWithEmailAndPassword } from "firebase/auth";
-// import {auth} from "../../Firebase"
-    
+import { createUserWithEmailAndPassword, onAuthStateChanged, sendEmailVerification } from "firebase/auth";
+import { auth } from "../../Firebase"
+
 
 
 export const SignAPI = (data) => {
-    // console.log(data);
-    // return new Promise((resolve, reject) => {
-    //     createUserWithEmailAndPassword(auth, data.email, data.password)
-    //         .then((userCredential) => {
-    //             // Signed in 
-    //             const user = userCredential.user;
-    //             console.log(user);
-    //             // ...
-    //         })
-    //         .catch((error) => {
-    //             const errorCode = error.code;
-    //             const errorMessage = error.message;
-    //             console.log(errorCode);
-    //             // ..
-    //         });
-    // })
+    console.log(data);
+    return new Promise((resolve, reject) => {
+        createUserWithEmailAndPassword(auth, data.email, data.password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user);
+
+                onAuthStateChanged(auth, (user) => {
+                    if (user) {
+                        sendEmailVerification(user)
+                        // User is signed in, see docs for a list of available properties
+                        // https://firebase.google.com/docs/reference/js/firebase.User
+                        const uid = user.uid;
+                        // ...
+                    } else {
+                        // User is signed out
+                        // ...
+                    }
+                })
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode);
+                // ..
+            });
+    })
 }
